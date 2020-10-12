870131873e524ada755434f359f65d81ae547b70
Mayur Chhapra <mayur@thesoftwaremachine.in>
2020-10-12 18:17:47 +0530

    Basic setup with required files

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * Author: Mayur
+ * This is the server file with all the requried configurations
+ */
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+import "./src/config/database";
+import routes from "./src/routes/index";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+routes(app);
+
+app.listen(3000, () => {
+  console.log("Application has been started on PORT:", process.env.PORT);
+});
---
+++ src/controller/v1/service/v1/connection.service.js
@@ -0,0 +1,15 @@
+/**
+ * Author: Mayur
+ */
+
+const conn = (data, callback) => {
+  try {
+    return callback(null, data);
+  } catch (e) {
+    return callback(e, null);
+  }
+};
+
+module.exports = {
+  conn,
+};
