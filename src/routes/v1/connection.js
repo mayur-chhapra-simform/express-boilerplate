6fcb1f31174ce11693c9159111fdafdb83389d47
Mayur Chhapra <mayur@thesoftwaremachine.in>
2020-10-12 18:32:15 +0530

    Added Routes

---
+++ src/routes/v1/connection.js
@@ -0,0 +1,13 @@
+/**
+ * Author: Mayur
+ */
+
+import { Router } from "express";
+
+const router = Router();
+
+import controller from "../../controller/v1/connection.controller";
+
+router.get("/", controller.conn);
+
+module.exports = router;
